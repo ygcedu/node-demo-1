345e9620b5a88895cfa03bd911eb2cad463a9ea2
Joseph <937601471@qq.com>
2021-03-20 14:31:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,17 +24,27 @@ var server = http.createServer(function(request, response){
     if(path === '/'){
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
+        response.write(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+              <link rel="stylesheet" href="/x">
+            </head>
+            <body>
+            <h1>1级标题</h1>
+            </body>
+            </html>
+        `)
         response.end()
     } else if(path === '/x'){
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
+        response.write(`h1{color: red}`)
         response.end()
     } else {
         response.statusCode = 404
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
+        response.write(`你访问的页面不存在`)
         response.end()
     }
 
